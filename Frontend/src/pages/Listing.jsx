b24db88fd6bd5f4e72aaaea94f150379ef476305
Muhammad Yasir <myasir47991@gmail.com>
2024-03-23 23:41:31 +0500

    fixed some bugs

--- Frontend/src/pages/Listing.jsx
+++ Frontend/src/pages/Listing.jsx
@@ -43,6 +43,7 @@ export default function Listing() {
         setListing(data.data);
         setLoading(false);
         setError(false);
+        console.log(data.data);
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -102,10 +103,10 @@ export default function Listing() {
               {listing.address}
             </p>
             <div className='flex gap-4'>
-              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+              {!listing.offer && <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                 {listing.rent && 'For Rent'}
                 {listing.sell && 'For Sale'}
-              </p>
+              </p> }
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                   ${+listing.regularPrice - +listing.discountPrice} OFF
